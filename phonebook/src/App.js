400311ed594aea8373ec817c71bdd034133e6277
TamasSallai <tamas.sallai.hu@gmail.com>
2022-10-07 19:58:33 +0200

    added axios to fetch the data

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import PersonList from './PersonList'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ])
+  const [persons, setPersons] = useState([])
   const [name, setName] = useState('')
   const [number, setNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:3001/persons')
+      .then((response) => setPersons(response.data))
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
