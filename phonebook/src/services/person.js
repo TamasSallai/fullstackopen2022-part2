0cfa6607de82ebd0e2282ea2fb2160c16986952b
Tamas Sallai <tamas.sallai.hu@gmail.com>
2022-10-17 12:01:29 +0200

    the app is now deployable with the fitting backend

--- phonebook/src/services/person.js
+++ phonebook/src/services/person.js
@@ -1,25 +1,24 @@
 import axios from 'axios'
 
+const baseUrl = '/api/persons'
+
 const getAll = () => {
-  const response = axios.get('http://127.0.0.1:3001/persons')
+  const response = axios.get(baseUrl)
   return response.then((response) => response.data)
 }
 
 const create = (newObject) => {
-  const response = axios.post('http://127.0.0.1:3001/persons', newObject)
+  const response = axios.post(baseUrl, newObject)
   return response.then((response) => response.data)
 }
 
 const modify = (id, modifiedObject) => {
-  const response = axios.put(
-    `http://127.0.0.1:3001/persons/${id}`,
-    modifiedObject
-  )
+  const response = axios.put(`${baseUrl}/${id}`, modifiedObject)
   return response.then((response) => response.data)
 }
 
 const remove = (id) => {
-  const response = axios.delete(`http://127.0.0.1:3001/persons/${id}`)
+  const response = axios.delete(`${baseUrl}/${id}`)
   return response.then((response) => response)
 }
 
