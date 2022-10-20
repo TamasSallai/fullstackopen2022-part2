1bfd132f945452149e39883189df5cd30893cb62
TamasSallai <tamas.sallai.hu@gmail.com>
2022-10-20 14:31:01 +0200

    localhost to proxy, and modified the urls

--- phonebook/src/services/person.js
+++ phonebook/src/services/person.js
@@ -1,25 +1,22 @@
 import axios from 'axios'
 
 const getAll = () => {
-  const response = axios.get('http://127.0.0.1:3001/persons')
+  const response = axios.get('api/persons')
   return response.then((response) => response.data)
 }
 
 const create = (newObject) => {
-  const response = axios.post('http://127.0.0.1:3001/persons', newObject)
+  const response = axios.post('api/persons', newObject)
   return response.then((response) => response.data)
 }
 
 const modify = (id, modifiedObject) => {
-  const response = axios.put(
-    `http://127.0.0.1:3001/persons/${id}`,
-    modifiedObject
-  )
+  const response = axios.put(`api/persons/${id}`, modifiedObject)
   return response.then((response) => response.data)
 }
 
 const remove = (id) => {
-  const response = axios.delete(`http://127.0.0.1:3001/persons/${id}`)
+  const response = axios.delete(`api/persons/${id}`)
   return response.then((response) => response)
 }
 
